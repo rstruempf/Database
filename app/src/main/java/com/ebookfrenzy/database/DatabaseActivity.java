58061eeb7baddd01731345d5d2cd3052c318da0c
Ron Struempf <rstruem1@my.westga.edu>
2016-02-23 21:17:19 -0500

    Finished chapter 38 practice

--- app/src/main/java/com/ebookfrenzy/database/DatabaseActivity.java
+++ app/src/main/java/com/ebookfrenzy/database/DatabaseActivity.java
@@ -8,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +23,17 @@ public class DatabaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_database);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        idView = (TextView)findViewById(R.id.productID);
+        productBox = (EditText)findViewById(R.id.productName);
+        quantityBox = (EditText)findViewById(R.id.productQuantity);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         fab.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                         .setAction("Action", null).show();
+             }
+         });
     }
 
     @Override
@@ -49,4 +57,73 @@ public class DatabaseActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private int getQuantity() {
+        try {
+            return Integer.parseInt(quantityBox.getText().toString());
+        }
+        catch (NumberFormatException ex) {
+            return -1;
+        }
+    }
+
+    public void newProduct(View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        String productName = productBox.getText().toString();
+        if (productName.length() < 1) {
+            idView.setText("Name required");
+            return;
+        }
+
+        if (dbHandler.findProduct(productName) != null) {
+            idView.setText("Product exists");
+            return;
+        }
+
+        int quantity = getQuantity();
+
+        if (quantity <= 0) {
+            idView.setText("Quantity required");
+            return;
+        }
+
+        Product product = new Product(productName, quantity);
+
+        dbHandler.addProduct(product);
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct(View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+    public void removeProduct(View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+
+        if (result) {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        }
+        else {
+            idView.setText("No Match Found");
+        }
+    }
+
 }
---
+++ app/src/main/java/com/ebookfrenzy/database/MyDBHandler.java
@@ -0,0 +1,114 @@
+package com.ebookfrenzy.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Database handler
+ *
+ * Created by Ron on 2/23/2016.
+ */
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    public static final String TABLE_PRODUCTS = "products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                       int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_PRODUCTS +
+                "(" + COLUMN_ID + " INTEGER PRIMARY KEY," +
+                      COLUMN_PRODUCTNAME + " TEXT," +
+                      COLUMN_QUANTITY + " INTEGER" +
+                ")";
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // if a newer version is installed, delete the table and recreate
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    /**
+     * Add a product to the database
+     *
+     * @param product Product to add
+     */
+    public void addProduct(Product product) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.getProductName());
+        values.put(COLUMN_QUANTITY, product.getQuantity());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    /**
+     * Lookup a product by name
+     *
+     * @param name Product name to locate
+     * @return Product found or null
+     */
+    public Product findProduct(String name) {
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =  \"" + name + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Product product = new Product();
+
+        if (!cursor.moveToFirst()) {
+            db.close();
+            return null;
+        }
+
+        //cursor.moveToFirst();
+        product.setID(Integer.parseInt(cursor.getString(0)));
+        product.setProductName(cursor.getString(1));
+        product.setQuantity(Integer.parseInt(cursor.getString(2)));
+        cursor.close();
+        db.close();
+        return product;
+    }
+
+    /**
+     * Delete a product record
+     *
+     * @param name Name of product to delete
+     * @return True if product record deleted
+     */
+    public boolean deleteProduct(String name) {
+        boolean result = false;
+
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =  \"" + name + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        Product product = new Product();
+
+        if (cursor.moveToFirst()) {
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?",
+                    new String[] { String.valueOf(product.getID()) });
+            cursor.close();
+            result = true;
+        }
+        db.close();
+        return result;
+    }
+}
